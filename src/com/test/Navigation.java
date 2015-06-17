7944cf57cb0f742897d5e00082f9699ad76df78d
scatlet007 <2736620075@qq.com>
2015-06-17 21:13:09 +0800

    在com.test中添加导航栏类。

---
+++ src/com/test/Navigation.java
@@ -0,0 +1,58 @@
+package com.test;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class Navigation extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Navigation frame = new Navigation();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Navigation() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel main = new JPanel();
+		contentPane.add(main);
+		main.setLayout(new BorderLayout(0, 0));
+		
+		JMenuBar menuBar = new JMenuBar();
+		main.add(menuBar, BorderLayout.NORTH);
+		
+		JMenu file = new JMenu("file");
+		menuBar.add(file);
+		
+		JMenuItem logout = new JMenuItem("logout");
+		file.add(logout);
+	}
+
+}
